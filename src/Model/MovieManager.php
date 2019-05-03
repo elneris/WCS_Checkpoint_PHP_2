1327768ece8b87627f84a92e7c52e789b691095e
elneris <46535320+elneris@users.noreply.github.com>
2019-05-03 23:26:28 +0200

    Incompréhension totale
    je ne sais absolument pas pourquoi j'ai supprimé ceci lorsque j'ai voulu faire mes modifs pour le test unitaires ... ???

--- src/Model/MovieManager.php
+++ src/Model/MovieManager.php
@@ -57,4 +57,25 @@ class MovieManager extends AbstractManager
 
         $statement->execute();
     }
+    
+    /**
+     * Get movie_id from database join beast.
+     *
+     * @param  int $id
+     *
+     * @return array
+     */
+    public function selectMovieByBeastIdMovie(int $id)
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("
+            SELECT movie.title as title 
+            FROM $this->table 
+            JOIN beast
+            ON beast.id_movie = movie.id
+            WHERE id_movie=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetch();
+    }
 }
