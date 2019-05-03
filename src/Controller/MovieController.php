8a3da49912774d64a1ec2079f830f80b5b74dbb4
Elneris Dang <elneris.dang@gmail.com>
2019-05-03 12:35:37 +0200

    bonnus pour moi crud movie

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\MovieManager;
+use App\Model\PlanetManager;
 
 class MovieController extends AbstractController
 {
@@ -17,5 +18,117 @@ class MovieController extends AbstractController
         $moviesManager = new MovieManager();
         $movies = $moviesManager->selectAll();
         return $this->twig->render('Movie/list.html.twig', ['movies' => $movies]);
+    }
+
+    /**
+     * @param int $id
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function details(int $id)  : string
+    {
+        $movieManager = new MovieManager();
+        $movie = $movieManager->selectOneById($id);
+
+        return $this->twig->render('Movie/details.html.twig', [
+            'movie' => $movie,
+        ]);
+    }
+
+
+    /**
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add()  : string
+    {
+        $moviesManager = new MovieManager();
+
+        $errors = [];
+        $success = [];
+
+        if ($_SERVER['REQUEST_METHOD'] = 'POST'){
+            if (empty($_POST['title'])){
+                $errors[] = 'Veuillez Renseigner TOUT les champs';
+            } else {
+                foreach ($_POST as $key => $value){
+                    $_POST[$key] = $this->checkForm($value);
+                }
+
+                $moviesManager->insert($_POST);
+                $success[] = 'ajout bien effectués';
+
+            }
+
+        }
+
+
+        return $this->twig->render('Movie/add.html.twig', [
+            'errors' => $errors,
+            'success' => $success
+        ]);
+    }
+
+
+    /**
+     *
+     * @param int $id
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function edit(int $id) : string
+    {
+
+        $movieManager = new MovieManager();
+
+        $errors = [];
+        $success = [];
+
+        if (isset($_POST['delete'])){
+            $movieManager->delete($id);
+            header('location: /Movie/list');
+        }
+        if ($_SERVER['REQUEST_METHOD'] = 'POST'){
+
+            if (empty($_POST['title']) ){
+                $errors[] = 'Veuillez Renseigner TOUT les champs';
+            } else {
+                foreach ($_POST as $key => $value){
+                    $_POST[$key] = $this->checkForm($value);
+                }
+
+                $movieManager->update($_POST, $id);
+                $success[] = 'Changement bien effectués';
+
+            }
+
+        }
+
+        $movie = $movieManager->selectOneById($id);
+
+        return $this->twig->render('Movie/edit.html.twig',[
+            'movie' => $movie,
+            'errors' => $errors,
+            'success' => $success
+        ]);
+    }
+
+    /**
+     *
+     * @param string $str
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function checkForm(string $str):string
+    {
+        return htmlspecialchars(trim($str));
     }
 }
--- src/Model/MovieManager.php
+++ src/Model/MovieManager.php
@@ -40,4 +40,43 @@ class MovieManager extends AbstractManager
 
         return $statement->fetch();
     }
+
+    /**
+     * update Beast from database.
+     *
+     * @param array $informations
+     * @param int $id
+     *
+     * @return void
+     */
+    public function update(array $informations, int $id):void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("
+            UPDATE $this->table
+            SET title = :title
+            WHERE id = :id");
+        $statement->bindValue(':title',$informations['title'],\PDO::PARAM_STR);
+        $statement->bindValue(':id',$id,\PDO::PARAM_INT);
+
+        $statement->execute();
+    }
+
+    /**
+     * insert Beast from database.
+     *
+     * @param array $informations
+     *
+     * @return void
+     */
+    public function insert(array $informations):void
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("
+            INSERT INTO $this->table(title)
+            VALUES (:title)");
+        $statement->bindValue(':title',$informations['title'],\PDO::PARAM_STR);
+
+        $statement->execute();
+    }
 }
